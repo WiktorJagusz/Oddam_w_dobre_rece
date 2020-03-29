7e8b4028ce1284e40daea9a6b5544614cf4c41a5
Wiktor Jagusz <wiktor.jagusz@icloud.com>
2020-03-29 23:03:39 +0200

    configuration

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Witaj świecie</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
